91d8e3e9a7c29b85f79caef94bb2f9e85a270ec6
Durga Potnuru <vijaypotnuru123@gmail.com>
2025-04-17 02:42:40 +0530

    Add migration entry for version 5 with tag '0009_stiff_chameleon'

---
+++ migrations/0009_stiff_chameleon.sql
@@ -0,0 +1,109 @@
+DO $$ BEGIN
+ CREATE TYPE "aal_level" AS ENUM('aal3', 'aal2', 'aal1');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "action" AS ENUM('ERROR', 'TRUNCATE', 'DELETE', 'UPDATE', 'INSERT');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "code_challenge_method" AS ENUM('plain', 's256');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "equality_op" AS ENUM('in', 'gte', 'gt', 'lte', 'lt', 'neq', 'eq');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "factor_status" AS ENUM('verified', 'unverified');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "factor_type" AS ENUM('webauthn', 'totp');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "key_status" AS ENUM('expired', 'invalid', 'valid', 'default');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "key_type" AS ENUM('stream_xchacha20', 'secretstream', 'secretbox', 'kdf', 'generichash', 'shorthash', 'auth', 'hmacsha256', 'hmacsha512', 'aead-det', 'aead-ietf');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "pricing_plan_interval" AS ENUM('year', 'month', 'week', 'day');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "pricing_type" AS ENUM('recurring', 'one_time');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ CREATE TYPE "subscription_status" AS ENUM('unpaid', 'past_due', 'incomplete_expired', 'incomplete', 'canceled', 'active', 'trialing');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "customers" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"stripe_customer_id" text
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "prices" (
+	"id" text PRIMARY KEY NOT NULL,
+	"product_id" text,
+	"active" boolean,
+	"description" text,
+	"unit_amount" bigint,
+	"currency" text,
+	"type" "pricing_type",
+	"interval" "pricing_plan_interval",
+	"interval_count" integer,
+	"trial_period_days" integer,
+	"metadata" jsonb
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "products" (
+	"id" text PRIMARY KEY NOT NULL,
+	"active" boolean,
+	"name" text,
+	"description" text,
+	"image" text,
+	"metadata" jsonb
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"full_name" text,
+	"avatar_url" text,
+	"billing_address" jsonb,
+	"updated_at" timestamp with time zone,
+	"payment_method" jsonb,
+	"email" text
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "prices" ADD CONSTRAINT "prices_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
